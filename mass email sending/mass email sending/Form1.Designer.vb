ff8b197075efd227280b08082eca416fea576d82
Terence Lui <wushu.dude@gmail.com>
2013-03-29 19:44:21 -0400

    commiting the first master branch

--- mass email sending/mass email sending/Form1.Designer.vb
+++ mass email sending/mass email sending/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Emailsend
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,29 +22,34 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Button1
+        'TextBox1
         '
-        Me.Button1.Location = New System.Drawing.Point(129, 96)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(284, 262)
+        Me.TextBox1.TabIndex = 0
         '
-        'Form1
+        'Emailsend
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
+        Me.ControlBox = False
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Emailsend"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
         Me.Text = "Form1"
+        Me.TopMost = True
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
--- mass email sending/mass email sending/Form1.vb
+++ mass email sending/mass email sending/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Imports System.Net.Mail
-Public Class Form1
+Public Class Emailsend
     Public emailcount As Integer = 5
     Public greetings() As String
     Public face As String = "(´･ω･`)"
@@ -9,43 +9,28 @@ Public Class Form1
 
 
     Sub Start() Handles Me.Load
-        greetings = Split("hi,hello,hey,how are you", ",")
-        bye = Split("bye,farewell,see ya,love,what,kay", ",")
-        body = Split("styff, more stuff a shole, a whole sentence, you know, I don't even know what I'm saying", ",")
-
-
         SmtpServer.Credentials = New  _
 Net.NetworkCredential("wushu.dude@gmail.com", "B7jahaDR")
         SmtpServer.Port = 587
         SmtpServer.Host = "smtp.gmail.com"
         SmtpServer.EnableSsl = True
-        Randomize()
     End Sub
 
 
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+    Private Sub sendoffitem(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.FormClosed
         Dim i As Integer = 0
-        Do While i < emailcount
-            Try
-                Dim mail As New MailMessage()
-                mail = New MailMessage()
-                mail.From = New MailAddress("myemail@gmail.com")
-                mail.To.Add("sansin@live.ca")
-                mail.To.Add("wushu.dude@gmail.com")
-                mail.Subject = greetings(Int(greetings.Length * Rnd()))
-                mail.Body = bye(Int(bye.Length * Rnd())) + vbCrLf + face
-                SmtpServer.Send(mail)
-                i += 1
-                System.Threading.Thread.Sleep(1000 * 7.2 * 60)
-            Catch ex As Exception
-                MsgBox(ex.ToString)
-            End Try
-        Loop
-    End Sub
-
-    Sub createemail()
-
-
+        Try
+            Dim mail As New MailMessage()
+            mail = New MailMessage()
+            mail.From = New MailAddress("myemail@gmail.com")
+            mail.To.Add("wushu.dude@gmail.com")
+            mail.Subject = "Subject"
+            mail.Body = TextBox1.Text
+            SmtpServer.Send(mail)
+            i += 1
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
     End Sub
 
 End Class
